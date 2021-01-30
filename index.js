dfdf3ed4b0f8fa6c1102833804a50b437f8e4a34
shahzad133 <34775920+shahzad133@users.noreply.github.com>
2021-01-29 17:03:10 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,61 @@
+for (i=0; i < document.querySelectorAll(".drum").length; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        soundGenerator(this.innerHTML); 
+    })
+}
+
+document.addEventListener("keypress", function(event) {
+    soundGenerator(event.key);
+})
+
+
+function soundGenerator (e) {
+    switch(e) {
+        case "w":
+        case "W":
+            var audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
+        break;
+
+        case "a":
+        case "A":
+            var audio = new Audio("sounds/tom-2.mp3");
+            audio.play();
+        break;
+
+        case "s":
+        case "S":
+            var audio = new Audio("sounds/tom-3.mp3");
+            audio.play();
+        break;
+
+        case "d":
+        case "D":
+            var audio = new Audio("sounds/tom-4.mp3");
+            audio.play();
+        break;
+
+        case "j":
+        case "J":
+            var audio = new Audio("sounds/snare.mp3");
+            audio.play();
+        break;
+
+        case "k":
+        case "K":
+            var audio = new Audio("sounds/crash.mp3");
+            audio.play();
+        break;
+
+        case "l":
+        case "L":
+            var audio = new Audio("sounds/kick-bass.mp3");
+            audio.play();
+        break;
+
+        default:
+            console.log(this.e);
+        break;
+    }    
+}
+
